7710b44199ecdc9b44e7e1bb216154473b401b88
emisenhe <72117384+emisenhe@users.noreply.github.com>
2023-02-08 23:06:45 +0000

    Working Updates (yay!)

--- scripts/rank.sh
+++ scripts/rank.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # Mode 2: Rank Modules
-# echo $1 will return a file path 
-echo $1
+# echo $1 will return a file path only if 
+# file path is github 
+GITPATH = 'github'
+if [["$1" == *"$GITPATH"*]]; then echo $1
+else echo "Based on our requirements we will not proces this file because it has a npm file path"
--- scripts/run.c
+++ scripts/run.c
@@ -1,8 +1,12 @@
 // C file that calls the bash scripts through input parsing
+// https://www.cs.uleth.ca/~holzmann/C/system/shell_commands.html#:~:text=FILE%20*fp%3B%20char%20*command,fp%20%3D%20popen%20(command%2C%22
 
 #include <stdlib.h> 
 
-int main() {
-    return 0
+char getCLIInput() {
+    return()
 }
 
+void main() {
+    getCLIInput()
+}
