8fa0b0e01ae0ff321aa7ba168f42405ad5e3a9b0
emisenhe <72117384+emisenhe@users.noreply.github.com>
2023-02-12 23:37:37 +0000

    Logfile work in progress

--- tool/log/logfile.rs
+++ tool/log/logfile.rs
@@ -1,17 +1,23 @@
 use log::{error, info, LevelFilter};
-use std::fs::OpenOptions;
+use std::fs::File;
+use std::io::{BufReader, Read};
 use std::io::Write;
 
-fn main() {
-  int verbosity = std::env::var("LOG_LEVEL").unwrap_or_else(|_| "info".to_string());
-  let log_level = match verbosity
+let env_path = "https://github.com/PurdueSoftEng/CLI-Tool/blob/main/tool/.env.txt";
+let env_file = File::open(env_path).unwrap(); 
+let mut reader = BufReader::new(env_file);
+let verbosity = reader.line().filter(|line| line.unwrap().contains("LOG_LEVEL"));
+let log_level: i32 = verbosity.split_whitespace()
+  .filter(|word| word.parse::<i32>().is_ok())
+  .map(|word| word.trim().parse::<i32>().unwrap())
+  .next()
+  .unwrap();
 
-  let mut log_file = OpenOptions::new()
-    .create(true)
-    .append(true)
-    .open("log_file.txt")
-    .unwrap()
+let mut log_file = OpenOptions::new()
+  .create(true)
+  .append(true)
+  .open("log_file.txt")
+  .unwrap()
 
-  log::set_boxed_logger(Box::new(WriteLogger::new(log_level, log::LogConfig::default(), log_file))).unwrap();
-  log::set_max_level(log_level);
-}
+log::set_boxed_logger(Box::new(WriteLogger::new(log_level, log::LogConfig::default(), log_file))).unwrap();
+log::set_max_level(log_level);
